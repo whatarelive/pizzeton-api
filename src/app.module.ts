3dde7abbdbfe5dd8abe87908fcb12991be8a7591
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2024-12-07 22:02:45 +0100

    Creación del modulo de archivos.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { ProductModule } from './product/product.module';
 import { OpinionsModule } from './opinions/opinions.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [ProductModule, OpinionsModule, AuthModule, CommonModule],
+  imports: [
+    ProductModule,
+    OpinionsModule,
+    AuthModule,
+    CommonModule,
+    FilesModule,
+  ],
   controllers: [],
   providers: [],
 })
