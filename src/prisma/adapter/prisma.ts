547a079c01d67f6b373d446a0d6beb25ea880053
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2024-12-23 17:15:07 +0100

    Prueba

--- src/prisma/adapter/prisma.ts
+++ src/prisma/adapter/prisma.ts
@@ -2,7 +2,7 @@ import { createClient } from '@libsql/client';
 import { PrismaLibSQL } from '@prisma/adapter-libsql';
 
 export const libsql = createClient({
-  url: 'file:./prisma/dev.db',
+  url: 'file:./dev.db',
   syncUrl: process.env.TURSO_DATABASE_URL,
   authToken: process.env.TURSO_AUTH_TOKEN,
   syncInterval: 60,
