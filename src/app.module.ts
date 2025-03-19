66a27d0a88a3b98060cba04853c2f2d3d7971803
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2025-03-19 02:26:19 +0100

    Refactorización y documentación JSDOC del módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,29 @@
 import { Module } from '@nestjs/common';
-import { ProductModule } from './product/product.module';
-import { OpinionsModule } from './opinions/opinions.module';
-import { AuthModule } from './auth/auth.module';
-import { CommonModule } from './common/common.module';
-import { FilesModule } from './files/files.module';
-import { SeedModule } from './seed/seed.module';
-import { EventsModule } from './events/events.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { ProductModule } from 'src/product/product.module';
+import { OpinionsModule } from 'src/opinions/opinions.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { CommonModule } from 'src/common/common.module';
+import { FilesModule } from 'src/files/files.module';
+import { SeedModule } from 'src/seed/seed.module';
+import { EventsModule } from 'src/events/events.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
+/**
+ * Módulo Principal
+ *
+ * @description Este módulo maneja la integración de módulos de
+ * toda la aplicación
+ */
 @Module({
   imports: [
-    AuthModule,
-    CommonModule,
-    FilesModule,
-    ProductModule,
-    OpinionsModule,
-    EventsModule,
-    SeedModule,
-    PrismaModule,
+    AuthModule, // Módulo de autentificación
+    CommonModule, // Módulo de funcionalidades comunes
+    FilesModule, // Módulo de manejo de archivos
+    ProductModule, // Módulo de Productos
+    OpinionsModule, // Módulo de Opiniones
+    EventsModule, // Módulo de Eventos
+    SeedModule, // Módulo seed de la Base de datos
+    PrismaModule, // Módulo de configuración del ORM de la Base de datos
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from 'src/app.module';
 import { ValidationPipe } from '@nestjs/common';
-// import { libsql } from './prisma/adapter/prisma';
 
 async function bootstrap() {
+  // Creación del punto de inicio de la aplicación
   const app = await NestFactory.create(AppModule);
 
+  // Prefijo inicial a la url del servicio
   app.setGlobalPrefix('api');
 
+  // Habilitación para que solo se puedan hacer peticiones desde un
+  // origen determinado
   app.enableCors({
     origin: '*',
   });
 
+  // Configuración global de validación de param y props de las requests
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -23,6 +27,7 @@ async function bootstrap() {
     }),
   );
 
+  // Se inicia la aplicación el puerto disponible
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
